6ee2f88ca4542db5abc01910bc005cae31532d09
Gavin Brown <scareb15@gmail.com>
2017-11-03 18:40:34 -0600

    heroku deploy

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
 const config = require('../knexfile')
-const knex = require('knex')
-const db = knex(config.development)
-module.exports = db
+const env = process.env.NODE_ENV || 'development'
+
+module.exports = require('knex')(config[env])
