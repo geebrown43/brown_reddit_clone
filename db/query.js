7db9d1bbd02f04ecc41abd1dec30ad6e78754dba
Ellis Mwekunda <Scareb15@gmail.com>
2017-11-06 11:59:56 -0700

    added function for post and get of links

--- db/query.js
+++ db/query.js
@@ -4,12 +4,21 @@ getAllUsers = () => {
     return db.select('*').from('users')
 }
 
+getAllLinks = () => {
+    return db.select('*').from('links')
+}
+
 createUser = (data) => {
     return db('users').insert(data)
 }
 
+createLink = (data) => {
+    return db('links').insert(data)
+}
 
 module.exports = {
     getAllUsers,
-    createUser
+    createUser,
+    createLink,
+    getAllLinks
 }
