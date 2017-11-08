9ee4722a58048893d290806bce4c6ad23559e2f6
Ellis Mwekunda <Scareb15@gmail.com>
2017-11-07 21:11:49 -0700

    deploy

---
+++ public/login.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let id1 = event.target.elements.email.value
+    let id2 = event.target.elements.password.value
+
+    funcLogin(id1,id2)
+})
+
+funcLogin = (id1, id2) => {
+    fetch(`http://localhost:3000/api/redditusers/user/${id1},${id2}`)
+    .then((response) => {
+        return response.json()
+        .then((data) => {
+            const login_status = document.getElementsByClassName('login_status')[0]
+            let username = data[0].username
+            let tag = document.createElement('p')
+                tag.textContent = 'Logged in as:' + username
+                tag.className = 'navbar-item'
+            login_status.appendChild(tag)
+        })
+    })
+    return form.innerHTML = ''
+}
+
