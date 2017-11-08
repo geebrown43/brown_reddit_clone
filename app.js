f7f61fb45fe40b97eaec913139949c69dffe2c2a
Ellis Mwekunda <Scareb15@gmail.com>
2017-11-07 21:07:48 -0700

    added new routes

--- app.js
+++ app.js
@@ -12,27 +12,34 @@ app.use(express.static('public'))
 app.use('/',routing)
 
 
-app.get('/api/redditusers', (req, res) => {
-    db.getAllUsers(req.body)
+app.get('/api/redditusers/user/:email,:password', (req, res) => {
+    const email = req.params.email
+    const password = req.params.password
+    console.log(req.params)
+    db.getUserByUserNameandPassword(email, password)
     .then((users) => res.json(users))
+    .catch(err => res.json(err))
 })
+
 app.get('/api/redditusers/link', (req, res) => {
     db.getAllLinks(req.body)
-    .then((links) => res.json(links))
+    .then((links) => res.render('myprofile.hbs',data))
 })
+
 app.post('/api/redditusers', (req, res, next) => {
-    let data = {
-        username: req.body.username,
-        email: req.body.email
-    }
     db.createUser(req.body)
-    .then(user => res.render('myprofile.hbs', data))
-    console.log(data.username)
+    .then(user => res.redirect('login.hbs'))
+    
 
 })
 app.post('/api/redditusers/link', (req, res) => {
+    let data = {
+        name: req.body.name,
+        link: req.body.link,
+        votes: req.body.votes
+    }
     db.createLink(req.body)
-    .then(link => res.sendStatus(201).json(link))
+    .then(link => res.render('myprofile.hbs', data))
 })
 
 app.listen(port, () => {
