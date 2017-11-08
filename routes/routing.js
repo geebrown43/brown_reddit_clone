e2e2fbf46e1274dc43e41af33291347b5d206cc8
Ellis Mwekunda <Scareb15@gmail.com>
2017-11-07 21:10:56 -0700

    changed routes

--- routes/routing.js
+++ routes/routing.js
@@ -1,28 +1,31 @@
 const express = require('express')
 const router = express.Router();
+const db = require('../db/query')
+
 
 router.get('/', (req, res) => {
     res.render('index')
+    
 })
 
 router.get('/create.hbs', (req, res) => {
     res.render('create')
 })
 
-router.get('/index.hbs', (req, res) => {
-    res.render('index')
-})
 
 router.get('/login.hbs', (req, res) => {
     res.render('login')
 })
 
 router.get('/api/myprofile.hbs', (req, res) => {
+    console.log(req.body)
     res.render('myprofile')
+    
 })
 
-router.get('/api/index.hbs', (req, res) => {
-    res.render('index')
+router.get('/api/login.hbs', (req, res) => {
+    res.render('login')
 })
+
 
 module.exports = router
